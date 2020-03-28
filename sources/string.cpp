9ef1cbea4c4a0d717905ce012c5f34caf2cfa989
ezuryy <daria.kidinova@gmail.com>
2020-03-28 22:49:01 +0300

    memory16

--- sources/string.cpp
+++ sources/string.cpp
@@ -45,7 +45,7 @@ String &String::operator+=(const String &rhs) {
         *(temp + temp_len + i) = *(rhs.Data + i);
     }
     Len += rhs.Len;
-    delete[] Data;
+    delete Data;
     Data = new char[Len];
     std::copy(temp, temp + Len, Data);
     delete[] temp;
@@ -55,7 +55,7 @@ String &String::operator+=(const String &rhs) {
 String &String::operator*=(unsigned int m) {
     String temp(*this);
     Len = 0;
-    delete[] Data;
+    delete Data;
     Data = new char();
     for (unsigned int i = 0; i < m; ++i) {
          *this += temp;
